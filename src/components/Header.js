5dbfbd45b009d7544f48e0134f37412d026c6d83
Tyler Schroeder <tschroeder@surgeforward.com>
2020-01-29 22:03:43 -0800

    site updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,18 +4,23 @@ import Footer from './Footer'
 import avatar from '../assets/images/avatar.jpg'
 
 class Header extends React.Component {
-    render() {
-        return (
-            <header id="header">
-                <div className="inner">
-                    <a href="/" className="image avatar"><img src={avatar} alt="" /></a>
-                    <h1>Hi, I am <strong>Tyler Schroeder</strong>.<br />
-                    I am an engineering manager and<br /> full stack web developer<br /> based out of Bellingham, WA.</h1>
-                </div>
-                <Footer />
-            </header>
-        )
-    }
+  render() {
+    return (
+      <header id="header">
+        <div className="inner">
+          <img class="image avatar" src={avatar} alt="Tyler Schroeder avatar" />
+          <h1>
+            Hi, I am <strong>Tyler Schroeder</strong>.<br />
+            I am a tech lead &amp;
+            <br />
+            web application developer
+            <br /> based out of Bellingham, WA.
+          </h1>
+        </div>
+        <Footer />
+      </header>
+    )
+  }
 }
 
 export default Header
